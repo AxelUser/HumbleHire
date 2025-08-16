bb7e87c23ae8c74b3f26d17f4dbd3f26b64e561c
Alexey Maltsev <axel.user.perm@gmail.com>
2025-08-16 03:19:57 +0200

    feat: integrate posthog-js

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,18 @@
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
+import * as env from '$env/static/public';
+import { dev } from '$app/environment';
+
 export const prerender = true;
+
+export const load = async () => {
+	if (browser && !dev && env.PUBLIC_POSTHOG_API_KEY && env.PUBLIC_POSTHOG_HOST) {
+		posthog.init(env.PUBLIC_POSTHOG_API_KEY, {
+			api_host: env.PUBLIC_POSTHOG_HOST,
+			defaults: '2025-05-24',
+			person_profiles: 'always'
+		});
+	}
+
+	return;
+};
